6278b1209a6ecfd7688d3d2c08ef7af34c8ccc97
Muhammad Mughni Aziz <mughniiaziz@gmail.com>
2021-12-23 13:42:37 +0700

    update ssl option

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,9 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  dialectOption: true,
+  dialectOptions: {
+    ssl: true,
+  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
